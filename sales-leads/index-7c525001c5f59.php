e8d9d8ab53ef2873a6033420ab1e3d310b0089a4
James Flynn <james.flynn@example.com>
2024-12-21 01:06:16 -0500

    Added CSS and PHP backend files

---
+++ sales-leads/index-7c525001c5f59.php
@@ -0,0 +1,1 @@
+{"data":{},"recaptcha":null,"recaptcha_invisible":{"public_key":"6LdtpIUUAAAAANYECIeArjTEvAb9n1-HoECIAh4P","private_key":"6LdtpIUUAAAAAFlbRgBGh8Ev1FG9TkpdDNUury4w","theme":"","size":"","badge":"bottomleft"},"hcaptcha":{"site_key":"563d4461-c13f-4259-995a-b09b0114e50c","secret_key":"0x5845FF6f007073659017260315D71e7102543337","theme":"light","invisible":true}}
